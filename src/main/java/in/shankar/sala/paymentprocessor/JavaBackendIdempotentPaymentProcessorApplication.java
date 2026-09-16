76affa4def23764eae20e14f31ac0d6b8409e9be
Shankar Sala <shankarsala9995@gmail.com>
2026-09-16 13:15:44 +0530

    chore: initialize Spring Boot payment processor project

---
+++ src/main/java/in/shankar/sala/paymentprocessor/JavaBackendIdempotentPaymentProcessorApplication.java
@@ -0,0 +1,14 @@
+package in.shankar.sala.paymentprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendIdempotentPaymentProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendIdempotentPaymentProcessorApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/in/shankar/sala/paymentprocessor/JavaBackendIdempotentPaymentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shankar.sala.paymentprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendIdempotentPaymentProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
